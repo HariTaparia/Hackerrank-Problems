21bc7904bf95cf1e3e324756530ec8189be1bd2b
HariTaparia <tapariahari@gmail.com>
2019-10-07 21:26:01 +0530

    Update find _a_string.py

--- find _a_string.py
+++ find _a_string.py
@@ -1,13 +1,12 @@
-In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+# In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
 
-NOTE: String letters are case-sensitive.
+# NOTE: String letters are case-sensitive.
 
-Input Format
+# Input Format
+# he first line of input contains the original string. The next line contains the substring.
 
-The first line of input contains the original string. The next line contains the substring.
-
-
-
+# CODE STARTS HERE
+----------------------------------------
 def count_substring(string, sub_string):
     count = 0
     l1 = len(string) 
@@ -17,3 +16,4 @@ def count_substring(string, sub_string):
             count +=1
     return count
 if __name__ == '__main__':
+-----------------------------------------
