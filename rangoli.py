507c0baa87a08942b70d3a1769a7c03a6cdd7b36
HariTaparia <tapariahari@gmail.com>
2019-10-08 19:43:21 +0530

    Update rangoli.py

--- rangoli.py
+++ rangoli.py
@@ -1,26 +1,26 @@
-You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+# You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
 
-Different sizes of alphabet rangoli are shown below:
+# Different sizes of alphabet rangoli are shown below:
 
-#size 3
+# #size 3
 
-----c----
---c-b-c--
-c-b-a-b-c
---c-b-c--
-----c----
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
 
-#size 5
+# #size 5
 
---------e--------
-------e-d-e------
-----e-d-c-d-e----
---e-d-c-b-c-d-e--
-e-d-c-b-a-b-c-d-e
---e-d-c-b-c-d-e--
-----e-d-c-d-e----
-------e-d-e------
---------e--------
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
 
 
 -------------------------
