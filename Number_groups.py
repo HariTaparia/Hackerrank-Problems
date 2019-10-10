71935b2bce9cd073efce5657fa295acfdc821c0a
HariTaparia <tapariahari@gmail.com>
2019-10-11 00:17:55 +0530

    Update Number_groups.py

--- Number_groups.py
+++ Number_groups.py
@@ -1,11 +1,11 @@
-The positive odd numbers are sorted in ascending order as , and grouped as  and so on.
+# The positive odd numbers are sorted in ascending order as , and grouped as  and so on.
 
-Thus, the first group is , the second group is , the third group is , etc. In general, the  group contains the next  elements of the sequence.
+# Thus, the first group is , the second group is , the third group is , etc. In general, the  group contains the next  elements of the sequence.
 
-Given , find the sum of the elements of the  group. For example, for , the answer is :
+# Given , find the sum of the elements of the  group. For example, for , the answer is :
 
-image
-##Rationale - As you see that each term is forming a pattern in terms of cube(k) or you can form the equation that kth term will have (n+1)*n th ending terms then k-1 th term has n*(n-1) th ending term and we know that sum of odd terms form a sequence of square so going by that we can sum like this and answer is (n*(n+1))*(n*(n+1))-(n*(n-1))*(n*(n-1))
+# image
+# ##Rationale - As you see that each term is forming a pattern in terms of cube(k) or you can form the equation that kth term will have (n+1)*n th ending terms then k-1 th term has n*(n-1) th ending term and we know that sum of odd terms form a sequence of square so going by that we can sum like this and answer is (n*(n+1))*(n*(n+1))-(n*(n-1))*(n*(n-1))
 -------------------------------------
 
 import math
